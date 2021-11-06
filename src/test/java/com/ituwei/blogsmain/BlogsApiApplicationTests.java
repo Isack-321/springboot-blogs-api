0a1fbf801a2d88a0a7c531f8a3e21a3a222b70e5
Isack <ituwei21@gmail.com>
2021-11-06 21:10:33 +0300

    getting started

---
+++ src/test/java/com/ituwei/blogsmain/BlogsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ituwei.blogsmain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
